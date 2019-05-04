9f1d9980375ca213500afaeceed2c88f72f6e6c7
Masahiro KIYOOKA <kiyookamasa@gmail.com>
2019-05-04 14:24:33 +0900

    rename dotfiles

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 SCRIPT_DIR="${PWD}/"`dirname $0`
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -fs $SCRIPT_DIR/.vimrc ~/.vimrc
-ln -fs $SCRIPT_DIR/.gvimrc ~/.gvimrc
+ln -fs $SCRIPT_DIR/vimrc ~/.vimrc
+ln -fs $SCRIPT_DIR/gvimrc ~/.gvimrc
 
 export XDG_CONFIG_HOME=$HOME/.config
 export XDG_CACHE_HOME=$HOME/.cache
@@ -18,7 +18,7 @@ mkdir -p $XDG_DATA_HOME
 mkdir -p $XDG_RUNTIME_DIR
 mkdir -p $XDG_CONFIG_HOME/nvim
 ln -fs $SCRIPT_DIR/init.vim $XDG_CONFIG_HOME/nvim/init.vim
-ln -fs $SCRIPT_DIR/.bash_profile $HOME/.bash_profile
-ln -fs $SCRIPT_DIR/.bashrc $HOME/.bashrc
-ln -fs $SCRIPT_DIR/.screenrc $HOME/.screenrc
+ln -fs $SCRIPT_DIR/bash_profile $HOME/.bash_profile
+ln -fs $SCRIPT_DIR/bashrc $HOME/.bashrc
+ln -fs $SCRIPT_DIR/screenrc $HOME/.screenrc
 ln -fs $SCRIPT_DIR/gitconfig $HOME/.gitconfig
