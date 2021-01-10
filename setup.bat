b3519905e250e1fcaadfd2ee4336b7fe40addddb
Masahiro KIYOOKA <kiyookamasa@gmail.com>
2021-01-11 00:21:55 +0900

    Change from `copy` to `link`

--- setup.bat
+++ setup.bat
@@ -1,13 +1,14 @@
 curl -fLo %HOME%\vimfiles\autoload\plug.vim --create-dirs ^
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-REM for vim and gvim
-xcopy /E %HOME%\initfile\bash_profile %HOME%\.bash_profile
-xcopy /E %HOME%\initfile\bashrc       %HOME%\.bashrc
-xcopy /E %HOME%\initfile\vimrc        %HOME%\.vimrc
-xcopy /E %HOME%\initfile\gvimrc       %HOME%\.gvimrc
-xcopy /E %HOME%\initfile\screenrc     %HOME%\.screenrc
-xcopy /E %HOME%\initfile\gitconfig    %HOME%\.gitconfig
+mklink %HOME%\.bash_profile %HOME%\.initfile\bash_profile
+mklink %HOME%\.bashrc %HOME%\.initfile\bashrc
+mklink %HOME%\.vimrc %HOME%\.initfile\vimrc
+mklink %HOME%\.gvimrc %HOME%\.initfile\gvimrc
+mklink %HOME%\.screenrc %HOME%\.initfile\screenrc
+mklink %HOME%\.tmux.config %HOME%\.initfile\tmux.config
+mklink %HOME%\.gitconfig %HOME%\.initfile\gitconfig
+mklink %HOME%\.gitignore %HOME%\.initfile\gitignore
 
+mkdir -p ~/doc/wiki
 vim -c PlugInstall -c q -c q
-
